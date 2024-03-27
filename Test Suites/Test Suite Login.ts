e06a282cbf0651302c1751364bcf82f1466fcb7b
thsangtran <thanhsangtran3010@gmail.com>
2024-03-27 10:03:32 +0700

    Commited

---
+++ Test Suites/Test Suite Login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b79d2262-1cf2-45a5-9235-14e1e6ad042c</testSuiteGuid>
+   <testCaseLink>
+      <guid>826c47e2-393a-4520-94ef-fc090d89b4e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>a980125a-5db7-466b-842f-95015a165546</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test Data Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>a980125a-5db7-466b-842f-95015a165546</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>680a4470-c5ce-4f9b-a286-c886101e9d7b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>a980125a-5db7-466b-842f-95015a165546</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>0e9ba82d-d5ee-463f-891b-94194240c1f2</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
